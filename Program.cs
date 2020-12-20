1241b808f8fcda76bbb1a86b5374705d1c240b39
Piotr Radlak <p10trek@o2.pl>
2020-12-20 15:39:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using chen0040.ExpertSystem;
+using NUnit.Framework;
+
+
+namespace ExpertSystem
+{class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Jakie obuwie?");
+
+            List<Clause> unproved_conditions = new List<Clause>();
+            RoleEngine.getInferenceEngine();
+            Clause conclusion = null;
+            while (conclusion == null)
+            {
+                Console.WriteLine("Jaki Sezon? \nWiosna:1\nLato:2\nJesień:3\nZima:4\n");
+                int sezon = RoleLoop(new List<int> { 1, 2, 3, 4 });         
+                Console.WriteLine("Jaka pogoda? \nSłoneczna:1\nPochmurna:2\nDeszczowa:3\nMroźna:4\n");
+                int pogoda = RoleLoop(new List<int> { 1, 2, 3, 4});
+                Console.WriteLine("Jaka okazja? \nSpacer:1\nBieg:2\nTurystyka:3\nOficjalna:4\n");
+                int okazja = RoleLoop(new List<int> { 1, 2, 3, 4});
+                Console.WriteLine("Jaka płeć?\nKobieta:1\nMężczyzna:2\n");
+                int plec = RoleLoop(new List<int> { 1, 2 });
+
+                RoleEngine.rie.CheckRule((RoleEngine.Sezon) sezon, (RoleEngine.Pogoda) pogoda, (RoleEngine.Przeznaczenie) okazja, (RoleEngine.Płeć) plec);
+                conclusion = RoleEngine.rie.Infer("Rodzaj Buta", unproved_conditions);
+                foreach(Clause clause in unproved_conditions)
+                {
+                    Console.WriteLine($"Brak dopasowania dla:{clause}");
+                }
+            }
+
+            Console.WriteLine("Konkluzja: " + conclusion.Value);
+            Console.ReadLine();
+        }
+        public static int RoleLoop(List<int> CorrectParams)
+        {
+            int wybor = 0;
+            while (!CorrectParams.Contains(wybor))
+            {
+                try
+                {
+                    wybor = Convert.ToInt32(Console.ReadLine());
+                    if (CorrectParams.Contains(wybor))
+                    {
+                        return wybor;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nieprawidlowy wybór");
+                        continue;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Nieprawidlowy wybór");
+                    continue;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ RoleEngine.cs
@@ -0,0 +1,94 @@
+﻿using chen0040.ExpertSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpertSystem
+{
+	static class RoleEngine
+	{
+		public enum Sezon
+		{
+			Wiosna,
+			Lato,
+			Jesień,
+			Zima
+		}
+        public enum Pogoda
+		{
+			Słoneczna,
+			Pochmurna,
+			Deszczowa,
+			Mroźna
+        }
+        public enum Przeznaczenie
+		{
+			Spacer,
+			Bieg,
+			Turystyka,
+			Oficjalne
+        }
+		public enum Płeć
+		{
+			Kobieta,
+			Mężczyzna
+		}
+		public static RuleInferenceEngine rie = new RuleInferenceEngine();
+		public static void getInferenceEngine()
+		{
+			Console.WriteLine("Start systemu");
+			rie.AddRule("Botki", Sezon.Jesień, Pogoda.Deszczowa, Przeznaczenie.Oficjalne, Płeć.Kobieta);
+			rie.AddRule("Botki", Sezon.Zima, Pogoda.Deszczowa, Przeznaczenie.Oficjalne, Płeć.Kobieta);
+			rie.AddRule("Botki", Sezon.Zima, Pogoda.Mroźna, Przeznaczenie.Oficjalne, Płeć.Kobieta);
+			rie.AddRule("Baleriny", Sezon.Lato, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Sandały", Sezon.Lato, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Czułenka", Sezon.Wiosna, Pogoda.Słoneczna, Przeznaczenie.Oficjalne, Płeć.Kobieta);
+			rie.AddRule("Czułenka", Sezon.Lato, Pogoda.Słoneczna, Przeznaczenie.Oficjalne, Płeć.Kobieta);
+			rie.AddRule("Trampki", Sezon.Lato, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Trampki", Sezon.Lato, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Adidasy", Sezon.Lato, Pogoda.Pochmurna, Przeznaczenie.Bieg, Płeć.Kobieta);
+			rie.AddRule("Adidasy", Sezon.Lato, Pogoda.Pochmurna, Przeznaczenie.Bieg, Płeć.Mężczyzna);
+			rie.AddRule("Kozaki", Sezon.Zima, Pogoda.Mroźna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Mokasyny", Sezon.Lato, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Oksfordy", Sezon.Lato, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Mokasyny", Sezon.Jesień, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Oksfordy", Sezon.Jesień, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Mokasyny", Sezon.Lato, Pogoda.Słoneczna, Przeznaczenie.Oficjalne, Płeć.Mężczyzna);
+			rie.AddRule("Oksfordy", Sezon.Lato, Pogoda.Pochmurna, Przeznaczenie.Oficjalne, Płeć.Mężczyzna);
+			rie.AddRule("Sneakersy", Sezon.Lato, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Sneakersy", Sezon.Jesień, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Sneakersy", Sezon.Wiosna, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Sneakersy", Sezon.Lato, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Sneakersy", Sezon.Jesień, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Sneakersy", Sezon.Wiosna, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Sneakersy", Sezon.Lato, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Sneakersy", Sezon.Jesień, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Sneakersy", Sezon.Wiosna, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Sneakersy", Sezon.Lato, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Sneakersy", Sezon.Jesień, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Sneakersy", Sezon.Wiosna, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Kobieta);
+			rie.AddRule("Trapery", Sezon.Zima, Pogoda.Słoneczna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Trapery", Sezon.Zima, Pogoda.Mroźna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Trapery", Sezon.Zima, Pogoda.Deszczowa, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+			rie.AddRule("Trapery", Sezon.Zima, Pogoda.Pochmurna, Przeznaczenie.Spacer, Płeć.Mężczyzna);
+		}
+		public static void AddRule(this RuleInferenceEngine ruleInference, string Rule_name, Sezon sezon, Pogoda pogoda, Przeznaczenie przeznaczenie, Płeć plec)
+		{
+			Rule rule = new Rule(Rule_name);
+			rule.AddAntecedent(new IsClause("Sezon",sezon.ToString()));
+			rule.AddAntecedent(new IsClause("Pogoda", pogoda.ToString()));
+			rule.AddAntecedent(new IsClause("Przeznaczenie", przeznaczenie.ToString()));
+			rule.AddAntecedent(new IsClause("Płeć", plec.ToString()));
+			rule.setConsequent(new IsClause("Rodzaj Buta", Rule_name));
+			ruleInference.AddRule(rule);
+		}
+		public static void CheckRule(this RuleInferenceEngine ruleInference, Sezon sezon, Pogoda pogoda, Przeznaczenie przeznaczenie, Płeć plec)
+        {
+			ruleInference.AddFact(new IsClause("Sezon", sezon.ToString()));
+			ruleInference.AddFact(new IsClause("Pogoda", pogoda.ToString()));
+			ruleInference.AddFact(new IsClause("Przeznaczenie", przeznaczenie.ToString()));
+			ruleInference.AddFact(new IsClause("Płeć", plec.ToString()));
+			ruleInference.Infer();
+		}
+	}
+}
